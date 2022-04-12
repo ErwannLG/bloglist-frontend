7e4da1d433a454207bf055e04e45e956593242a6
ErwannLG <erwannj.legall@gmail.com>
2022-04-12 11:34:53 +0200

    5.17: bloglist end to end testing, step1

--- src/App.js
+++ src/App.js
@@ -65,6 +65,7 @@ const App = () => {
         <div>
           username
           <input
+            id="username"
             type="text"
             value={username}
             name="Username"
@@ -74,6 +75,7 @@ const App = () => {
         <div>
           password
           <input
+            id="password"
             type="password"
             value={password}
             name="Password"
